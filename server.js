ce2db67330ceb25fcae776e73df44e6af874a5e4
isyrafimrani <isyrafimrani@gmail.com>
2017-08-16 06:39:04 -0400

    Added PUT request

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var ingredients = [
     {
@@ -25,18 +25,20 @@ var ingredients = [
     }
 ]
 
-app.get('/', function(req, res) {
+// Fetch data
+app.get('/ingredients', function (req, res) {
     res.send(ingredients);
 });
 
-app.get('/isyraf', function(req, res) {
+app.get('/isyraf', function (req, res) {
     res.send('Isyraf is the author of this API');
 });
 
-app.post('/', function(req, res) {
+// Add data
+app.post('/ingredients', function (req, res) {
     var ingredient = req.body;
     if (!ingredient || ingredient.text === "") {
-        res.status(500).send({error: "Your ingredient must have text"});
+        res.status(500).send({ error: "Your ingredient must have text" });
     }
     else {
         ingredients.push(ingredient);
@@ -44,6 +46,35 @@ app.post('/', function(req, res) {
     }
 });
 
-app.listen(4200, function() {
+// Update data
+app.put('/ingredients/:ingredientId', function (req, res) {
+
+    var ingredientId = req.params.ingredientId;
+    var newText = req.body.text;
+
+    if (!newText || newText == "") {
+        res.status(500).send({ error: "You must provide ingredient text" });
+    } else {
+        var objectFound = false;
+        for (var i = 0; i < ingredients.length; i++) {
+            var ingredient = ingredients[i];
+            if (ingredient.id === req.params.ingredientId) {
+                ingredients[i].text = newText;
+                objectFound = true;
+                break;
+            }
+        }
+
+        if (!objectFound) {
+            res.status(500).send({ error: "Ingredient ID not found!!!" });
+        } else {
+            res.send(ingredients);
+        }
+    }
+
+
+})
+
+app.listen(4200, function () {
     console.log("First API running on port 4200");
 });
